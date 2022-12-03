33815ca513a6bb7d884016eab28b2e243f8eedec
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-12-03 11:41:10 -0600

    added categories to gameCard

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -1,23 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { getGameCategories } from '../../API/CategoryData';
 import { getSingleGame } from '../../API/GameData';
 import GameCard from '../../components/GameCard';
 
 export default function ViewGame() {
   const [game, setGame] = useState({});
+  const [gameCategories, setGameCategories] = useState([]);
   const router = useRouter();
   const { id } = router.query;
 
   const getTheGame = () => {
     getSingleGame(id).then(setGame);
+    getGameCategories().then(setGameCategories);
   };
+
+  const thisGameCategories = () => gameCategories.filter((category) => category.game.id === game.id);
 
   useEffect(() => {
     getTheGame();
   }, [id]);
 
   return (
-    <GameCard game={game} />
+    <GameCard game={game} categories={thisGameCategories()} />
   );
 }
